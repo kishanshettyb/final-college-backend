37da5e0b8b903d3b23333a74b557d7381f58659d
Kishan B <kishanqr@gmail.com>
2025-11-21 12:00:36 +0530

    fix issues

--- src/api/mark/controllers/mark.js
+++ src/api/mark/controllers/mark.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
-/**
- * mark controller
- */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController("api::mark.mark", ({ strapi }) => ({
+	async create(ctx) {
+		const { data } = ctx.request.body;
+		const { subject, student, semister } = data;
 
-module.exports = createCoreController('api::mark.mark');
+		// Check if already exists
+		const existing = await strapi.db.query("api::mark.mark").findOne({
+			where: {
+				subject: { documentId: subject },
+				student: { documentId: student },
+				semister
+			}
+		});
+
+		if (existing) {
+			return ctx.badRequest("Marks already exist for this student + subject + semester");
+		}
+
+		// Continue default creation
+		return await super.create(ctx);
+	}
+}));
